fd4850a4c5476d477da699c228d5427b15fae06a
dhruv5433 <dhruvsuri67@gmail.com>
2026-06-04 14:11:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,34 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import sqlite3
 
 app = Flask(__name__)
 
 @app.route('/')
-def login():
+def home():
     return render_template('login.html')
+
+@app.route('/login', methods=['POST'])
+def login():
+
+    email = request.form['email']
+    password = request.form['password']
+
+    conn = sqlite3.connect('ticketing.db')
+    cursor = conn.cursor()
+
+    cursor.execute(
+        "SELECT * FROM users WHERE email=? AND password=?",
+        (email, password)
+    )
+
+    user = cursor.fetchone()
+
+    conn.close()
+
+    if user:
+        return render_template('dashboard_u.html')
+
+    return "Invalid Email or Password"
 
 if __name__ == '__main__':
     app.run(debug=True)

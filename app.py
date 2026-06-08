762197ed110105e5997b3190a3d49ec1bd871d91
dhruv5433 <dhruvsuri67@gmail.com>
2026-06-08 17:35:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, session, redirect, url_for
 import sqlite3
+from datetime import datetime
 
 app = Flask(__name__)
 
@@ -12,7 +13,7 @@ def home():
 @app.route('/login', methods=['POST'])
 def login():
 
-    email = request.form['email']
+    employee_id = request.form['employee_id']
     password = request.form['password']
 
     conn = sqlite3.connect('ticketing.db')
@@ -23,8 +24,8 @@ def login():
     print(cursor.fetchall())
 
     cursor.execute(
-    "SELECT * FROM users WHERE email=? AND password=?",
-    (email, password))
+    "SELECT * FROM users WHERE employee_id=? AND password=?",
+    (employee_id, password))
 
     user = cursor.fetchone()
     print(user)
@@ -34,7 +35,8 @@ def login():
     if user:
 
         session['user_id'] = user[0]
-        session['username'] = user[1]
+        session['username'] = user[5] + " " + user[6]
+        session['role'] = user[4]
 
         role = user[4]
 
@@ -50,7 +52,7 @@ def login():
         elif role == 'L3':
             return redirect('/l3-dashboard')
 
-    return "Invalid Email or Password"
+    return "Invalid Employee ID or Password"
 
 @app.route('/create-ticket')
 def create_ticket():
@@ -66,26 +68,46 @@ def submit_ticket():
     conn = sqlite3.connect('ticketing.db')
     cursor = conn.cursor()
 
+  
+    cursor.execute("SELECT COUNT(*) FROM tickets")
+    ticket_count = cursor.fetchone()[0]
+
+    l1_engineers = [6, 7, 8]  
+
+    assigned_to = l1_engineers[
+        ticket_count % len(l1_engineers)
+    ]
+
     cursor.execute("""
     INSERT INTO tickets
-    (title, description, status, assigned_level, created_by)
-    VALUES (?, ?, ?, ?, ?)
+    (title, description, status, assigned_level, assigned_to, created_by, ticket_code)
+    VALUES (?, ?, ?, ?, ?, ?, ?)
     """,
-    (title, description, 'Open', 'L1', session['user_id']))
+    (
+        title,
+        description,
+        'Open',
+        'L1',
+        assigned_to,
+        session['user_id'],
+        f"TKT-{ticket_count + 1:04d}"
+    ))
 
     conn.commit()
     conn.close()
 
     return redirect('/dashboard')
-   
-    conn.commit()
-    conn.close()
+
+from datetime import datetime
 
 @app.route('/register', methods=['POST'])
 def register():
 
-    name = request.form['name']
+    first_name = request.form['first_name']
+    last_name = request.form['last_name']
+    employee_id = request.form['employee_id']
     email = request.form['email']
+    phone_number = request.form['phone_number']
     password = request.form['password']
     confirm_password = request.form['confirm_password']
 
@@ -100,18 +122,53 @@ def register():
         (email,)
     )
 
-    existing_user = cursor.fetchone()
+    existing_email = cursor.fetchone()
 
-    if existing_user:
+    if existing_email:
         conn.close()
         return "Email already registered"
 
+    cursor.execute(
+        "SELECT * FROM users WHERE employee_id=?",
+        (employee_id,)
+    )
+
+    existing_employee = cursor.fetchone()
+
+    if existing_employee:
+        conn.close()
+        return "Employee ID already exists"
+
+    create_date = datetime.now().strftime("%d-%m-%Y %H:%M")
+
+    name = first_name + " " + last_name
+
     cursor.execute("""
     INSERT INTO users
-    (name, email, password, role)
-    VALUES (?, ?, ?, ?)
+    (
+        name,
+        first_name,
+        last_name,
+        employee_id,
+        email,
+        phone_number,
+        password,
+        role,
+        create_date
+    )
+    VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)
     """,
-    (name, email, password, 'USER'))
+    (
+        name,
+        first_name,
+        last_name,
+        employee_id,
+        email,
+        phone_number,
+        password,
+        'USER',
+        create_date
+    ))
 
     conn.commit()
     conn.close()
@@ -159,7 +216,11 @@ def l1_dashboard():
     cursor = conn.cursor()
 
     cursor.execute(
-        "SELECT * FROM tickets WHERE assigned_level='L1'"
+    """
+    SELECT * FROM tickets
+    WHERE assigned_to=?
+    """,
+    (session['user_id'],)
     )
 
     tickets = cursor.fetchall()
@@ -227,6 +288,173 @@ def l2_dashboard():
 @app.route('/l3-dashboard')
 def l3_dashboard():
     return "L3 Dashboard"
+
+@app.route('/view-ticket', methods=['POST'])
+def view_ticket():
+
+    ticket_id = request.form['ticket_id']
+
+    conn = sqlite3.connect('ticketing.db')
+    conn.row_factory = sqlite3.Row
+    cursor = conn.cursor()
+
+   
+    cursor.execute(
+        "SELECT * FROM tickets WHERE ticket_id=?",
+        (ticket_id,)
+    )
+
+    selected_ticket = cursor.fetchone()
+
+    cursor.execute(
+    """
+    SELECT
+    comments.*,
+    users.first_name,
+    users.last_name
+
+    FROM comments
+
+    JOIN users
+    ON comments.user_id = users.id
+
+    WHERE comments.ticket_id=?
+
+    ORDER BY comments.comment_id
+    """,
+    (ticket_id,)
+)
+
+    comments = cursor.fetchall()
+
+    cursor.execute(
+        """
+        SELECT * FROM tickets
+        WHERE assigned_to=?
+        """,
+        (session['user_id'],)
+    )
+
+    tickets = cursor.fetchall()
+
+    
+    cursor.execute(
+        """
+        SELECT * FROM users WHERE id=?
+        """,
+        (selected_ticket['created_by'],)
+    )
+
+    ticket_user = cursor.fetchone()
+
+    conn.close()
+
+    return render_template(
+        'l1_dashboard.html',
+        username=session['username'],
+        tickets=tickets,
+        selected_ticket=selected_ticket,
+        ticket_user=ticket_user,
+        comments=comments
+    )
+
+@app.route('/add-comment', methods=['POST'])
+def add_comment():
+
+    ticket_id = request.form['ticket_id']
+    comment = request.form['comment']
+
+    conn = sqlite3.connect('ticketing.db')
+    cursor = conn.cursor()
+
+    ticket_code = f"TKT-{int(ticket_id):04d}"
+    comment_date = datetime.now().strftime("%d-%m-%y %H:%M")
+    
+    cursor.execute(
+        """
+        INSERT INTO comments
+        (ticket_id, user_id, comment_by_role, comment, comment_date, ticket_code)
+        VALUES (?, ?, ?, ?, ?, ?)
+        """,
+        (
+            ticket_id,
+            session['user_id'],
+            session['role'],
+            comment,
+            comment_date,
+            ticket_code
+            
+        )
+    )
+
+    conn.commit()
+    conn.close()
+
+    if session['role'] == 'USER':
+        return redirect('/dashboard')
+    elif session['role'] == 'L1':
+        return redirect('/l1-dashboard')
+    elif session['role'] == 'L2':
+        return redirect('/l2-dashboard')
+    elif session['role'] == 'L3':
+        return redirect('/l3-dashboard')
+
+@app.route('/view-user-ticket', methods=['POST'])
+def view_user_ticket():
+
+    ticket_id = request.form['ticket_id']
+
+    conn = sqlite3.connect('ticketing.db')
+    conn.row_factory = sqlite3.Row
+    cursor = conn.cursor()
+
+    cursor.execute(
+        "SELECT * FROM tickets WHERE ticket_id=?",
+        (ticket_id,)
+    )
+
+    selected_ticket = cursor.fetchone()
+
+    cursor.execute(
+        """
+        SELECT
+            comments.*,
+            users.first_name,
+            users.last_name
+
+        FROM comments
+
+        JOIN users
+        ON comments.user_id = users.id
+
+        WHERE comments.ticket_id=?
+
+        ORDER BY comments.comment_id
+        """,
+        (ticket_id,)
+    )
+
+    comments = cursor.fetchall()
+
+    cursor.execute(
+        """
+        SELECT * FROM tickets
+        WHERE created_by=?
+        """,
+        (session['user_id'],)
+    )
+
+    tickets = cursor.fetchall()
+
+    conn.close()
+
+    return render_template(
+        'dashboard_u.html',
+        username=session['username'],
+        tickets=tickets,
+        selected_ticket=selected_ticket,
+        comments=comments
+    )
 
 if __name__ == '__main__':
     app.run(debug=True)

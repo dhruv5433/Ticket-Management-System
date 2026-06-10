f89cf71e9b8b0081f058f9c7b08a12cb006500f2
dhruv5433 <dhruvsuri67@gmail.com>
2026-06-10 13:08:38 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,74 @@
+function hideAllSections() {
+
+    let ticket = document.getElementById("ticket-details");
+    let user = document.getElementById("user-info");
+    let comments = document.getElementById("comments-section");
+    let actions = document.getElementById("actions-section");
+
+    if(ticket){
+        ticket.style.display = "none";
+    }
+
+    if(user){
+        user.style.display = "none";
+    }
+
+    if(comments){
+        comments.style.display = "none";
+    }
+
+    if(actions){
+        actions.style.display = "none";
+    }
+}
+
+function showTicket() {
+
+    hideAllSections();
+
+    let ticket = document.getElementById("ticket-details");
+
+    if(ticket){
+        ticket.style.display = "block";
+    }
+}
+
+function showUser() {
+
+    hideAllSections();
+
+    let user = document.getElementById("user-info");
+
+    if(user){
+        user.style.display = "block";
+    }
+}
+
+function showComments() {
+
+    hideAllSections();
+
+    let comments = document.getElementById("comments-section");
+
+    if(comments){
+        comments.style.display = "block";
+    }
+}
+
+function showActions() {
+
+    hideAllSections();
+
+    let actions = document.getElementById("actions-section");
+
+    if(actions){
+        actions.style.display = "block";
+    }
+}
+
+window.onload = function(){
+
+    if(document.getElementById("ticket-details")){
+        showTicket();
+    }
+};
